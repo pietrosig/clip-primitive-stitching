f24b98b7c404a040b65003b21f7321a5dcfd8874
Pietro <signorino9@gmail.com>
2025-04-04 17:56:00 +0200

    Add empty-pixel component to loss function

--- utils/cma.py
+++ utils/cma.py
@@ -8,4 +8,10 @@ def clip_sol(solution, max_val):
     # Make the solution to be within the range of 0-1
     solution = solution + max_val
 
+    # x, y in the [-0.5, 1] range
+    solution[:, 0:2] = solution[:, 0:2] * 1.5 - 0.5
+
+    # scale in the [0.2, 1.2] range
+    solution[:, 2] = solution[:, 2] * 1.2 + 0.2
+
     return solution
